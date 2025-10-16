372c2f2ba71b4b04176748d8c3f2cff22d674b3c
Manuela Di Dario <didariomanuela@gmail.com>
2025-10-16 13:36:11 +0200

    .gitignore updated

---
+++ .angular/cache/19.2.17/todo-app/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-AQWNUQVO.js";
+import "./chunk-RZBXDWZQ.js";
+import "./chunk-JHQ4UJML.js";
+import "./chunk-G3RBU73A.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
